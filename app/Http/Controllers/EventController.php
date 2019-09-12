d1e1843b247ff9256418a5b354a34e723ce43383
zhanghongjie-feel <2120155019@qq.com>
2019-09-12 13:59:31 +0800

    event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,6 +11,7 @@ class EventController extends Controller
      * 接收用户发送的消息【与用户互动，用户->微信端->开发者】
      */
     public function event(){
-        echo '这是微信访问开发者接口';
+        echo $_GET['echostr'];
+//        echo '这是微信访问开发者接口';
     }
 }
