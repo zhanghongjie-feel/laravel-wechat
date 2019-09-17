d01fce0d81ea39392ed987b88c60cdb33c4a2630
zhanghongjie-feel <2120155019@qq.com>
2019-09-17 18:48:31 +0800

    event2.7

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -48,10 +48,12 @@ class EventController extends Controller
                 }
 
             }
+        }else{
+            $message='欢迎关注';
+            $xml_str='<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
+            echo $xml_str;
         }
 
-        $message='欢迎关注';
-        $xml_str='<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
-        echo $xml_str;
+
     }
 }
